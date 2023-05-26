89906e2f09babd684b894ceca44a996f5eb6c2c4
fdelu <fdeluca@fi.uba.ar>
2023-05-26 02:04:43 -0300

    Reorganize backtracking code and comments

--- algoritmos/backtracking.py
+++ algoritmos/backtracking.py
@@ -20,10 +20,9 @@ def backtracking_rec(
 
     # Para cada objeto, probamos todas las posibilidades:
     # 1) Agregarlo a cualquiera de los paquetes existentes que entre
-    # 2) Agregarlo a un nuevo paquete
     for package in packages:
-        if len(packages) == new_best:  # Poda: por esta rama ya no mejora más
-            break
+        if len(packages) >= new_best:  # Poda: por esta rama ya no mejora más
+            return new_best
 
         if package.fits(objects[i]):
             package.add_object(objects[i])
@@ -31,11 +30,13 @@ def backtracking_rec(
             new_best = min(new_best, result)
             package.remove_object(objects[i])
 
-    if len(packages) + 1 < new_best:  # Poda
-        packages.append(Package())
-        packages[-1].add_object(objects[i])
-        result = backtracking_rec(objects, i + 1, packages, new_best, debug)
-        new_best = min(new_best, result)
-        packages.pop()
+    if len(packages) + 1 >= new_best:  # Poda
+        return new_best
 
+    # 2) Agregarlo a un nuevo paquete
+    packages.append(Package())
+    packages[-1].add_object(objects[i])
+    result = backtracking_rec(objects, i + 1, packages, new_best, debug)
+    new_best = min(new_best, result)
+    packages.pop()
     return new_best
